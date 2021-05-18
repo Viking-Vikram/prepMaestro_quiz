561497b86e134a2217de4ba89d95acf20d701f47
Viking-Vikram <kadarlavikky@gmail.com>
2021-05-18 09:13:00 +0530

    timer background

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
+"use strict";
+
 const labelTimer = document.querySelector(".timer");
-const timerContainer = document.querySelector(".timer-container");
 const drop = document.querySelector(".drop");
 const paragraph = document.querySelector(".paragraph");
+var qtContainer = document.querySelector(".qt-container");
+const timerContainer = document.querySelector(".timer-container");
 
 const startLogoutTimer = function () {
 	const tick = function () {
 		const min = Math.trunc(String(time / 60).padStart(2, 0));
 		const sec = String(time % 60).padStart(2, 0);
 
-		// In each call print time
 		labelTimer.textContent = `${min} Min : ${sec} Secs`;
 
+		if (time <= 10) {
+			timerContainer.style.backgroundColor = "red";
+		}
 		if (time === 0) {
 			clearInterval(timer);
 			labelTimer.textContent = "Time out";
 		}
 
-		// decrease timer by 1 second
 		time--;
 	};
-	// set time to 2 minutes
-	let time = 600;
-	// call timer every second
+
+	let time = 300;
+
 	tick();
 	const timer = setInterval(tick, 1000);
 	console.log(timer);
-	if (time <= 5) {
-		timerContainer.style.backgroundColor = "#e52165";
-	}
+
 	return timer;
 };
 startLogoutTimer();
+
+window.onscroll = function () {
+	if (
+		document.body.scrollTop >= 50 ||
+		document.documentElement.scrollTop >= 50
+	) {
+		qtContainer.classList.add("position");
+	} else {
+		qtContainer.classList.remove("position");
+	}
+};
 
 drop.addEventListener("click", () => {
 	paragraph.classList.toggle("show");
